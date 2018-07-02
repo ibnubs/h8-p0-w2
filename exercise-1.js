589dede37df7295dcd55318a00e9d5992fb71439
ibnubs <ibnubs@gmail.com>
2018-07-02 23:57:03 +0700

    update

---
+++ exercise-1.js
@@ -0,0 +1,17 @@
+/*Exercise-1 for Algoritma */
+
+/*soal 1*/
+
+Algoritma--
+
+//!--
+Mulai
+    1. input 'mass' (kg), 'acceleration' (m/s2), 'net force' (kg m/s2);
+    2. Simpan 'massa' benda dengan nilai = 600;
+    3. JIKA acceleration ==0;
+        3a. Ya. maka hasil net force =0;
+        3b. Jika tidak lanjut ke point 4;
+    4. HITUNG 'net force' = 'mass'*'acceleration'
+    5. Tampilkan (net force)
+end
+//
