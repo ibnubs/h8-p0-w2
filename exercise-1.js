561d1806b7d04ca5c6cfd9191948a4981620bcc9
ibnubs <ibnubs@gmail.com>
2018-07-03 11:46:11 +0700

    isi tugas exercise1

--- exercise-1.js
+++ exercise-1.js
@@ -7,7 +7,7 @@ Algoritma--
 //!--
 Mulai
     1. input 'mass' (kg), 'acceleration' (m/s2), 'net force' (kg m/s2);
-    2. Simpan 'massa' benda dengan nilai = 600;
+    2. Simpan 'mass' benda dengan nilai = 600;
     3. JIKA acceleration ==0;
         3a. Ya. maka hasil net force =0;
         3b. Jika tidak lanjut ke point 4;
@@ -15,3 +15,55 @@ Mulai
     5. Tampilkan (net force)
 end
 //
+
+/*soal2*/
+
+//!-- 
+Tahun Kabisat 
+
+Start;
+1. Input any value 'year';
+2. Jika 'year' / 400 = 0;
+    2.a Tampilkan 'Tahun Kabisat'; 
+    2.b Lanjutkan ke step 3;
+3. Jika 'year' / 400 = 0 and 'year'/ 100 = 0;
+    3.a Tampilkan 'Bukan Tahun Kabisat';
+    3.b Lanjutkan ke step 4;
+4.
+
+End
+--//
+
+/*soal3*/
+Laundry Day 
+
+Start
+1. Display 'Mesin Cuci On' jika nilai 'pakaian'= 20;
+2. Simpan nilai 'counter' start = 0;
+3. Lakukan SELAMA 'counter' lebih kecil dibanding 'pakaian';
+    3.a. 'Counter' ditambah 1;
+    3.b. Lanjut ke step 4;
+4.Program Selesai Tampilkan "Mesin Cuci On";
+
+END
+--//
+
+/*soal4*/
+Periksa Kuku
+
+Start
+inisialisasi
+'long' 'short' 'nails' 'student'
+1. SIMPAN 'student' with 0;
+2. LAKUKAN KETIKA 'student' < 40;
+3. TAMPILKAN PANGGIL 'student';
+    3.a. 'student' tambah 1;
+4. MASUKAN hasil cek 'nails' = 'short' atau 'long';
+5. SIMPAN 'nails' sebagai hasil pengencekan kuku siswa;
+6. Jika 'nails' = 'long'
+    6.a. TAMPILKAN 'hukuman'
+    6.b. Lanjutkan ke Step 7
+7. Jika 'nails' = 'short'
+    7.a. Tampilkan 'reward'    
+End
+--//
